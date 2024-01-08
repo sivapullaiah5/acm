aca46b6d3ef32e334fc0fff0e181efabf855d027
siva <msprocks992@gmail.com>
2024-01-08 23:07:34 +0530

    java-code

---
+++ student.java
@@ -0,0 +1,15 @@
+public class student
+{
+        public static void main(String args[]){
+                printstudentinfo();
+        }
+        private static void printstudentinfo(){
+                String collegename="aits";
+                String studentname="ramu";
+                int rollnumber=1596;
+        System.out.println("college name:"+collegename);
+        System.out.println("student name:"+studentname);
+        System.out.println("roll number:"+rollnumber);
+        System.out.println("welcome,"+studentname+"!");
+        System.out.println("we hope u have a fanatastic journey at "+collegename+".");
+        }}
